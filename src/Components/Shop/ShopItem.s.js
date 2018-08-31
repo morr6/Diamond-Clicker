b766392ff82449450d00bd473c764267db5a80f2
Marcin <elomelo302@gmail.com>
2018-08-31 12:17:53 +0200

    style changes

--- src/Components/Shop/ShopItem.s.js
+++ src/Components/Shop/ShopItem.s.js
@@ -81,12 +81,6 @@ export const ItemInfo = glamorous.div(props => {
         zIndex: 9999,
         top: props.clientY ? props.clientY - 90: 0,
         left: props.clientX ? props.clientX - 425 : 0,
-
-        '@media(max-width: 650px)': {
-            fontSize: 10,
-            height: '50px',
-            width: '116px'
-        }
     }
 })
 
